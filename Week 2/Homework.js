c288a2dcbcbeae52d884c2fa4178a0038a094f98
JNguWin <97162385+JNguWin@users.noreply.github.com>
2022-09-29 22:19:56 -0700

    Added functions

--- Week 2/Homework.js
+++ Week 2/Homework.js
@@ -1,56 +1,142 @@
-//Reference vs Value ===============================================
-let text = "Hello"
-let text2 = text
-console.log(text);
-console.log(text2);
+// //Reference vs Value ===============================================
+// let text = "Hello"
+// let text2 = text
+// console.log(text);
+// console.log(text2);
 
-text = "Bye"
-console.log(text);
-console.log(text2);
-
-
-
-let array = [10, 5, 2]
-let array2 = array
-console.log(array);
-console.log(array2);
-array[0] = 100;
-console.log(array);
-console.log(array2);
+// text = "Bye"
+// console.log(text);
+// console.log(text2);
 
 
 
-let person = {
-    firstName: "William",
-    lastName: "Cram"
+// let array = [10, 5, 2]
+// let array2 = array
+// console.log(array);
+// console.log(array2);
+// array[0] = 100;
+// console.log(array);
+// console.log(array2);
+
+
+
+// let person = {
+//     firstName: "William",
+//     lastName: "Cram"
+// }
+// let person2 = person;
+// console.log(person);
+// console.log(person2);
+
+// person2.firstName = "Mohamed";
+// console.log(person);
+// console.log(person2);
+
+
+
+
+// //Loops============================================================
+// let newArray = ["foo", "fooBar", "bar", "BarFoo"];
+// //Forloop
+// for(let i = 0; i < newArray.length; i++){
+//     console.log("Index: " + i + " Element: " + newArray[i]);
+// }
+
+// //While
+// let counter = 0;
+// while(counter < newArray.length){
+//     console.log("Index: " + counter + " Element: " + newArray[counter]);
+//     counter++
+// }
+
+// //ForEach
+// newArray.forEach((element, index) =>
+// console.log("Index: " + index + " Element: " + element));
+
+
+//Conditions=================================================================
+// let a = 20;
+// let b = "20";
+// let c = null;
+// let d;
+
+// if(a == b){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// if(a === b){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// if(c === d){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// if(c == 0){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// if(c == d){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// if(d){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// if(c){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+
+
+
+//Functions========================================================
+
+let input = prompt("Enter a number");
+while(input != Number){
+    input = prompt("Enter a number");
 }
-let person2 = person;
-console.log(person);
-console.log(person2);
-
-person2.firstName = "Mohamed";
-console.log(person);
-console.log(person2);
-
-
-
-
-//Loops============================================================
-let newArray = ["foo", "fooBar", "bar", "BarFoo"];
-//Forloop
-for(let i = 0; i < newArray.length; i++){
-    console.log("Index: " + i + " Element: " + newArray[i]);
+while(input == Number){
+    if(input%2 == 0){
+        console.log("userInput is even");
+    }
+    else if(input%3 == 0 || input == 1 || input == 0){
+        console.log("userInput is odd");
+    }
 }
 
-//While
-let counter = 0;
-while(counter < newArray.length){
-    console.log("Index: " + counter + " Element: " + newArray[counter]);
-    counter++
+
+//Original
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
 }
 
-//ForEach
-newArray.forEach((element, index) =>
-console.log("Index: " + index + " Element: " + element));
+//Function Expression
+let functionExpression = function(min, max){
+    return Math.floor(Math.random() * (max - min)) + min
+}
 
-
+//Arrow function
+let functionExpressions = (min, max) => Math.floor(Math.random() * (max - min)) + min;
