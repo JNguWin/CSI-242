3afd9da31c53d559d949f3707a3d1ecb2f23b27f
JNguWin <97162385+JNguWin@users.noreply.github.com>
2022-09-29 21:12:43 -0700

    just trying out codes for the homework

---
+++ Week 2/Homework.js
@@ -0,0 +1,33 @@
+let text = "Hello"
+let text2 = text
+console.log(text);
+console.log(text2);
+
+text = "Bye"
+console.log(text);
+console.log(text2);
+
+
+
+let array = [10, 5, 2]
+let array2 = array
+console.log(array);
+console.log(array2);
+array[0] = 100;
+console.log(array);
+console.log(array2);
+
+
+
+let person = {
+    firstName: "William",
+    lastName: "Cram"
+}
+let person2 = person;
+console.log(person);
+console.log(person2);
+
+person2.firstName = "Mohamed";
+console.log(person);
+console.log(person2);
+
