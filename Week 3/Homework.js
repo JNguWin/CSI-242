5803d9f7cf1fd5e18edea0799ae325d04da30311
JNguWin <97162385+JNguWin@users.noreply.github.com>
2022-10-07 23:44:17 -0700

    Homework notes

--- Week 3/Homework.js
+++ Week 3/Homework.js
@@ -39,20 +39,74 @@
 //If it is let or const then it cannot be defined outside and cant be returned
 
 
-function fy_10(){
-    window.x = 10;
-    var y = 30;
-    {
-        x = 11;
-        {
-            let x = 12
-            if(x === 12){
-                y = 13
-            }
-        }
-    }
-    return x + y;
+// function fy_10(){
+//     window.x = 10;
+//     var y = 30;
+//     {
+//         x = 11;
+//         {
+//             let x = 12
+//             if(x === 12){
+//                 y = 13
+//             }
+//         }
+//     }
+//     return x + y;
+// }
+
+
+// console.log(fy_10())
+
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(1, 4);
+// console.log(fruits)
+// console.log(citrus)
+
+
+
+// loop through array x and push each element to y if even
+// {
+//     let x = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+//     let y = [];
+
+//     for(let i = 0; i < x.length; i++){
+//         if(x[i] % 2 ==0){
+//             y.push(x[i])
+//         }
+//     }
+//     console.log(x)
+//     console.log(y)
+// } 
+
+
+
+// reverse the order of x using the array push/pop, shift/unshift methods
+// {
+//     let x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//     for(let i = 1; i < 11; i++){
+//         x.unshift(i)
+//     }
+//     for(let i = 0; i < 10; i++){
+//         x.pop()
+//     }
+    
+//     console.log(x)
+
+// }
+
+
+
+
+//splice the array above into two seperate arrays 
+//["f", "o", "o"] and ["b", "a", "r"]
+{
+    let names = ["f", "o", "o", "b", "a", "r"];
+    console.log(names)
+    let foo = names.splice(0, 3)
+    console.log(foo)
+    let bar = names.splice(0, 3)
+    console.log(bar)
+
 }
-
-
-console.log(fy_10())
