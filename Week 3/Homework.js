0f7749be124195a8ff2a4294473c4a2d57575f75
JNguWin <97162385+JNguWin@users.noreply.github.com>
2022-10-08 23:12:34 -0700

    Finished assignment

--- Week 3/Homework.js
+++ Week 3/Homework.js
@@ -65,6 +65,25 @@
 
 
 
+// Arrays
+// Explain push, pop, unshift, shift and splice methods of the array
+//Push - method that adds new items to the end of an array and adds to array length
+
+//Pop - method that removes the last element of an array, returns the removed element
+//and changes the original array
+
+//Unshift - method adds new elements to the beginning of an array
+
+//Shift - method removes the first element of the array
+
+//Splice - method that returns selected elements in an array as a new array
+//you give it a starting index and a number for how many to remove or you can put 0 and
+//add in elements by inputting the values after the 0
+
+
+
+
+
 // loop through array x and push each element to y if even
 // {
 //     let x = [2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -112,3 +131,101 @@
 // }
 
 
+
+//adding functionality to our basic reminder app
+// var reminderList = [
+//     {
+//         title : "Water the Garden",
+//         description : "Every other evening"
+//     },
+//     //Adding another object into the list
+//     {
+//         movie : "Horror",
+//         details : "Very scary"
+//     }
+// ]
+
+//reminderList is an array of objects [{}, {}, {}, ...]
+
+//a. what is the output of reminderList[0].title?
+//The output is "Water the garden"
+
+//b. what is the output of reminderList[0].description?
+//The output is "Every other evening"
+
+//c. what is the output of reminderList[0].['title']?
+//There is no output because it returned a syntax error that said
+//the [ token before title was unexpected
+
+//d. what is the output of reminderList[0].['description']?
+//Same thing as c there is no output because it returned a syntax error that said
+//the [ token before title was unexpected
+
+// console.log(reminderList[1].details)
+
+
+
+
+// var reminderList = [
+//     {
+//         title : "Water the Garden",
+//         description : "Every other evening"
+//     }
+//     Adding another object into the list, add a , and then new set of brackets
+//     ,{
+//         movie : "Horror",
+//         details : "Very scary"
+//     }
+// ]
+
+// function addReminder(){
+//     console.log("addReminder function");
+    //e. Get two input from user using prompt() for a title and description 
+    //and store them into an object with fields "title" and "description"
+    // let title = prompt("What is the title?")
+    // let description = prompt("What is the description")
+    // let object = {title, description}
+
+    //f. add this object to the array
+    // reminderList.push(object)
+    //reminderList.push({title, description})
+
+    //This link is for a video on how to do user input
+    //https://www.mikedane.com/web-development/javascript/getting-user-input/
+//}
+
+// addReminder()
+// console.log(reminderList)
+
+
+
+
+
+// var reminderList = [
+//     {
+//         title : "Water the Garden",
+//         description : "Every other evening"
+//     },
+//     {
+//         movie : "Horror",
+//         details : "Very scary"
+//     },
+//     {
+//         movie : "Action",
+//         details : "Everything blows up"
+//     }
+
+// ]
+
+// function removeReminder(index){
+//     console.log("removeReminder function", index);
+//     //g. Complete this function to remove an element in array reminderList 
+//     //using the index as the starting point.
+//     if(index > -1 && index < reminderList.length){
+//         reminderList.splice(index, 1) //The 1 indicates that we are to remove only 1
+//     }
+    
+// }
+// console.log(reminderList)
+// removeReminder(0)
+// console.log(reminderList)
